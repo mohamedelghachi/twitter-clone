343b8d931a074be30ab836bff6740467002bfbc5
ELGHACHI MOHAMED <Mohamed.Elghachi@4d.com>
2024-09-15 19:14:07 +0100

    1- post routes and controllers added

---
+++ backend/routes/post.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {createPost,deletePost,commentOnPost,likeUnlikePost,getAllPosts,getLikedPosts,getFollowingPosts,getUserPosts} from "../controllers/post.controller.js";
+import {protectRoute} from "../middleware/protectRoute.js";
+const router = express.Router();
+
+router.get("/all",protectRoute,getAllPosts)
+router.get("/userposts",protectRoute,getUserPosts)
+router.get("/likes/:id",protectRoute,getLikedPosts)
+router.get("/following",protectRoute,getFollowingPosts)
+router.post("/create",protectRoute,createPost)
+router.post("/like/:id",protectRoute,likeUnlikePost)
+router.post("/comment/:id",protectRoute,commentOnPost)
+router.delete("/:id",protectRoute,deletePost)
+
+export default router;
