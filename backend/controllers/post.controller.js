026d185448763785405c548c3ea0bb75d923fc5e
ELGHACHI MOHAMED <Mohamed.Elghachi@4d.com>
2024-09-16 23:12:01 +0100

    3 - like post mutation added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -99,7 +99,9 @@ export const likeUnlikePost = async (req, res) => {
     if (userLikedPost) {
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-      res.status(200).json({ message: "Post liked successfully" });
+
+      const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+      res.status(200).json(updatedLikes);
     } else {
       post.likes.push(userId);
       await post.save();
@@ -113,8 +115,8 @@ export const likeUnlikePost = async (req, res) => {
         to: post.user,
       });
       await newNotification.save();
-
-      res.status(200).json({ message: "Post unliked successfully" });
+      const updatedLikes = post.likes;
+      res.status(200).json(updatedLikes);
     }
   } catch (error) {
     console.log("Error in likeUnlikePost : ", error.message);
